0faa6662df149bfb7e64ed2998cf9114ff304655
Edward Yang <Edward_Yang@asus.com>
2021-10-05 09:51:19 +0800

    use pure pipe

--- src/app/core/components/user-card/user-card.component.ts
+++ src/app/core/components/user-card/user-card.component.ts
@@ -17,24 +17,4 @@ export class UserCardComponent implements OnInit {
   cardClicked() {
     this.router.navigate([`/users/${this.user.login.uuid}`]);
   }
-
-  createUniqueId(length) {
-    var result = '';
-    var characters =
-      'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    var charactersLength = characters.length;
-    for (var i = 0; i < length; i++) {
-      result += characters.charAt(Math.floor(Math.random() * charactersLength));
-    }
-    return result;
-  }
-
-  idUsingFactorial(num, length = 1) {
-    if (num === 1) {
-      return this.createUniqueId(length);
-    } else {
-      const fact = length * (num - 1);
-      return this.idUsingFactorial(num - 1, fact);
-    }
-  }
 }
